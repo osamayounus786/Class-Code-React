82f08d2b8cf99b7fb5d231014a8110f50c8fa289
Osama M.Younus <osamayounus541@gmail.com>
2022-11-15 15:18:24 +0500

    practicing React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+// function Osama(){
+//   const name = "Osama";
+//   const status = "ForntEnd Developer"
+//   return (
+//       <div className="bg">
+//         <p className="name">Hello Welcome {name}</p>
+//         <p className="status">Osama is {status}</p>
+//       </div>
+//   );
+// }
+
+// export default Osama;
+// const DATA = "Ghous"
+// function App() {
+//   // const email = "ghous@gmail.com";
+//   const arr = ["ghous", "ahmed", "khan"];
+//   return (
+//     <div>
+//       <h1>Hello World {DATA}</h1>
+//       <ul>
+//         {arr.map((v, i) => (
+//           <li key={i}>{v}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// function Arr(){
+//   const newArr = ['osama','younus','Baloch'];
+//   return(
+//     <div style={{backgroundColor: 'red'}}>
+//       <p>{newArr.map((b) =>{
+//           return `${b} ${b} `;
+//       })}</p>
+//     </div>
+//   )
+// }
+
+// export default Arr;
+import IMAGE from './assets/images/tom.jpg';
+import {Button as MyButton} from './components/index.js'
+import {SignUp}   from './components/index.js'
+
+
+function Header(){
+  return(
+    <div>
+     <img src={IMAGE} alt="tom"/> 
+     <MyButton />
+     <SignUp />
+     </div>
   );
 }
-
-export default App;
+export default Header;
---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+const  Button= ()=>{
+    return(
+    <button>Click Me</button>
+    )
+}
+
+
+const SignUp = ()=>{
+    return(
+        <button>SignUp</button>
+    )
+}
+
+export {Button,SignUp};
